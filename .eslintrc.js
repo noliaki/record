9d3286324634ec988102af7f6a9d63c5379e09b2
noliaki <noliaki@gmail.com>
2021-03-27 23:59:24 +0900

    wip

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react'],
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import React from 'react'
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>
+          Create Next
+          AppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppApp
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
