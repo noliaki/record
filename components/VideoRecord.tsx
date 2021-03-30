db8da10628b6c5147779bd4d8976017227289afd
noliaki <noliaki@gmail.com>
2021-03-31 00:03:00 +0900

    wip

--- components/VideoRecord.tsx
+++ components/VideoRecord.tsx
@@ -1,5 +1,21 @@
 import Head from 'next/head'
 import React, { useState, useRef, useCallback } from 'react'
+import {
+  load,
+  SupportedPackages,
+  FaceLandmarksPrediction,
+} from '@tensorflow-models/face-landmarks-detection'
+import '@tensorflow/tfjs-backend-webgl'
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BufferGeometry,
+  BufferAttribute,
+  MeshBasicMaterial,
+  Mesh,
+  DirectionalLight,
+} from 'three'
 
 export default function VideoRecord(): JSX.Element {
   const chunk = useRef<Blob[]>([])
@@ -7,9 +23,83 @@ export default function VideoRecord(): JSX.Element {
   const recordVideoEl = useRef<HTMLVideoElement | null>(null)
   const btnEl = useRef<HTMLButtonElement | null>(null)
   const mediaRecorder = useRef<MediaRecorder | null>(null)
+  const predictions = useRef<FaceLandmarksPrediction[] | null>(null)
+  const rendererEl = useRef<HTMLCanvasElement | null>(null)
+  const renderer = useRef<WebGLRenderer | null>(null)
+  const scene = useRef<Scene | null>(null)
+  const camera = useRef<PerspectiveCamera | null>(null)
+  const rafId = useRef<number | undefined>(undefined)
+  const geometry = useRef<BufferGeometry | null>(null)
+  const attribute = useRef<BufferAttribute | null>(null)
+  const material = useRef<MeshBasicMaterial | null>(null)
+  const mesh = useRef<Mesh | null>(null)
+
+  function createScene(): void {
+    if (rendererEl.current === null) {
+      return
+    }
+
+    renderer.current = new WebGLRenderer({
+      canvas: rendererEl.current,
+    })
+    scene.current = new Scene()
+    camera.current = new PerspectiveCamera(45, 560 / 960)
+    camera.current.position.z = 1000
+    camera.current.lookAt(0, 0, 0)
+
+    geometry.current = new BufferGeometry()
+    attribute.current = new BufferAttribute(new Float32Array([]), 3)
+    geometry.current.setAttribute('position', attribute.current)
+    material.current = new MeshBasicMaterial({
+      color: 0x00aaff,
+      wireframe: true,
+    })
+    mesh.current = new Mesh(geometry.current, material.current)
+    scene.current.add(mesh.current)
+
+    const directionalLight = new DirectionalLight(0xffffff)
+    directionalLight.position.set(1, 1, 1)
+    scene.current.add(directionalLight)
+
+    render()
+  }
+
+  function render(): void {
+    if (
+      renderer.current === null ||
+      scene.current === null ||
+      camera.current === null
+    ) {
+      return
+    }
+
+    if (predictions.current !== null) {
+      attribute.current = new BufferAttribute(
+        new Float32Array(
+          ((predictions.current[0] as any).scaledMesh as Array<
+            [number, number, number]
+          >).flat() ?? []
+        ),
+        3
+      )
+      console.log(predictions.current)
+      attribute.current.needsUpdate = true
+      if (geometry.current !== null) {
+        geometry.current.setAttribute('position', attribute.current)
+      }
+    }
+
+    renderer.current.render(scene.current, camera.current)
+
+    rafId.current = requestAnimationFrame(() => {
+      render()
+    })
+  }
 
   const onStartRecord = useCallback(() => {
     console.log('on start recort')
+
+    createScene()
   }, [])
 
   const onDataAvailable = useCallback((event: BlobEvent): void => {
@@ -37,6 +127,7 @@ export default function VideoRecord(): JSX.Element {
       return
     }
 
+    const model = await load(SupportedPackages.mediapipeFacemesh)
     videoEl.current.srcObject = stream
     await videoEl.current.play().catch(console.error)
 
@@ -47,6 +138,15 @@ export default function VideoRecord(): JSX.Element {
 
     chunk.current = []
     mediaRecorder.current.start(1)
+
+    if (rendererEl.current !== null) {
+      rendererEl.current.width = videoEl.current.videoWidth
+      rendererEl.current.height = videoEl.current.videoHeight
+    }
+
+    predictions.current = await model.estimateFaces({
+      input: videoEl.current,
+    })
   }
 
   function stopPlayAndRecord(): void {
@@ -58,7 +158,7 @@ export default function VideoRecord(): JSX.Element {
     mediaRecorder.current.stop()
   }
 
-  const onClick = useCallback((): void => {
+  const onClick = (): void => {
     console.log(chunk.current)
 
     if (videoEl.current?.paused === true) {
@@ -66,7 +166,7 @@ export default function VideoRecord(): JSX.Element {
     } else {
       stopPlayAndRecord()
     }
-  }, [])
+  }
 
   return (
     <React.Fragment>
@@ -78,6 +178,7 @@ export default function VideoRecord(): JSX.Element {
       <button ref={btnEl} onClick={onClick} type="button">
         ほげ
       </button>
+      <canvas ref={rendererEl}></canvas>
       <video ref={recordVideoEl} src=""></video>
     </React.Fragment>
   )
